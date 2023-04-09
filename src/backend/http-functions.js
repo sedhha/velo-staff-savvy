75a576404f556f5b47f06ab10b164aa31b2e5206
sedhha <activity.schoolsh2@gmail.com>
2023-04-09 15:21:49 +0530

    Added postEmail function

--- src/backend/http-functions.js
+++ src/backend/http-functions.js
@@ -1,8 +1,18 @@
 // eslint-disable-next-line @wix/cli/no-wix-extraneous-dependencies
-import { ok } from "wix-http-functions";
+import { ok, badRequest } from "wix-http-functions";
+import * as z from 'zod';
+
+const loginFormSchema = z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    emailAddress: z.string(),
+    orgCode: z.string(),
+    employeeCode: z.string(),
+    securePassword: z.string()
+})
 
 export function get_ping() {
-    let options = {
+    const options = {
         headers: {
             'Content-Type': 'application/json'
         },
@@ -11,4 +21,29 @@ export function get_ping() {
         }
     }
     return ok(options)
+}
+
+
+export function post_register_employee(request) {
+    const options = {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: {
+            message: 'pong'
+        }
+    }
+    const body = JSON.parse(request.body);
+    try {
+        const result = loginFormSchema.parse(body);
+        options.body = {
+            message: 'Success',
+            result
+        }
+        return ok(options);
+    }
+    catch (e) {
+        options.body = { message: e.message };
+        return badRequest(options);
+    }
 }
