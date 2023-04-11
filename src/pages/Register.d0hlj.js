65641c5b51eb65bb92ec6aa02312b700c409b7c3
sedhha <activity.schoolsh2@gmail.com>
2023-04-11 21:58:06 +0530

    Registration form working

--- src/pages/Register.d0hlj.js
+++ src/pages/Register.d0hlj.js
@@ -1,23 +1,29 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/1-hello-world
-
-const activeStyle = { color: 'white', background: 'black' };
-const inactiveStyle = { color: 'black', background: 'white' };
+// eslint-disable-next-line @wix/cli/no-invalid-backend-import
+import { signUpEmployees } from 'backend/authFunctions.jsw';
 
 $w.onReady(function () {
-    // Get references to the buttons
-    const signupButton = $w('#signUpButton');
-
-    signupButton.onClick(() => {
-        const firstName = $w('#firstNameFormInput').value;
-        const lastName = $w('#firstNameFormInput').value;
-        const emailAddress = $w('#emailFormInput').value;
-        const orgCode = $w('#emailFormInput').value;
-        const employeeCode = $w('#employeeCodeFormInput').value;
-        const securePassword = $w('#passwordFormInput').value;
-        console.log({ firstName, lastName, emailAddress, orgCode, employeeCode, securePassword })
-
-    })
-
-
+	// Add submit button click event
+	$w('#signupButton').onClick(() => {
+		const firstName = $w('#firstName').value;
+		const lastName = $w('#firstName').value;
+		const emailAddress = $w('#emailInput').value;
+		const employeeCode = $w('#employeeCode').value;
+		const securePassword = $w('#passwordInput').value;
+		const orgCode = '';
+		console.log('Requesting secure password for employee');
+		signUpEmployees({
+			firstName,
+			lastName,
+			emailAddress,
+			employeeCode,
+			securePassword,
+			orgCode,
+		})
+			.then((res) => {
+				console.log('Successfully added - ', res);
+			})
+			.catch((err) => {
+				console.error('Error adding - ', err);
+			});
+	});
 });
