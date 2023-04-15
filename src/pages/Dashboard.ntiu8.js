393d3655974ff26e2e8a04b1253db09cf77f6860
sedhha <activity.schoolsh2@gmail.com>
2023-04-15 08:01:29 +0530

    Updated Invites functionality

--- src/pages/Dashboard.ntiu8.js
+++ src/pages/Dashboard.ntiu8.js
@@ -22,9 +22,12 @@ const initSession = () => {
 	const userDetails = JSON.parse(session.getItem('user'));
 	const sessionDetails = JSON.parse(session.getItem('session'));
 	if (!userDetails || !sessionDetails) return wixLocation.to('/login');
+	if (!userDetails.user_metadata.orgAdmin)
+		return wixLocation.to('/dashboard-users');
 	$w('#nameField').text = `Hello ${
 		userDetails.user_metadata.firstName ?? 'Guest'
 	}`;
+	return sessionDetails.access_token;
 };
 
 $w.onReady(function () {
--- src/pages/Invites.hfnq1.js
+++ src/pages/Invites.hfnq1.js
@@ -1,10 +1,9 @@
-/* eslint-disable @wix/cli/no-invalid-backend-import */
+// /* eslint-disable @wix/cli/no-invalid-backend-import */
 import { session } from 'wix-storage';
 import wixLocation from 'wix-location';
 import {
-	generateMagicLink,
-	getAllPendingCode,
-	revokeAMagicCode,
+	getAllAccessesRequest,
+	approveAccessByRequestID,
 } from 'backend/adminFunctions.jsw';
 
 const showErrorMessage = (message) => {
@@ -24,74 +23,131 @@ const showGeneralMessage = (message) => {
 	infoText.expand();
 };
 
-let magicLinks = [];
+// let magicLinks = [];
 
-const fetchMagicLinks = (access_token) => {
-	getAllPendingCode(access_token)
-		.then(({ data }) => {
-			$w('#tableContent').data = [
-				...data.map((item, id) => ({
-					...item,
-					_id: `${id + 1 < 10 ? '0' : ''}${id + 1}`,
-				})),
-			];
-		})
-		.catch((err) => showErrorMessage(err.message));
-};
+// const fetchMagicLinks = (access_token) => {
+// 	getAllPendingCode(access_token)
+// 		.then(({ data }) => {
+// 			$w('#tableContent').data = [
+// 				...data.map((item, id) => ({
+// 					...item,
+// 					_id: `${id + 1 < 10 ? '0' : ''}${id + 1}`,
+// 				})),
+// 			];
+// 		})
+// 		.catch((err) => showErrorMessage(err.message));
+// };
 
-$w.onReady(function () {
+// $w.onReady(function () {
+// 	const userDetails = JSON.parse(session.getItem('user'));
+// 	const sessionDetails = JSON.parse(session.getItem('session'));
+// 	if (!userDetails || !sessionDetails) return wixLocation.to('/login');
+
+// 	$w('#nameField').text = `Hello ${
+// 		userDetails.user_metadata.firstName ?? 'Guest'
+// 	}`;
+
+// 	$w('#generateMagicLink').onClick(() => {
+// 		showGeneralMessage('Generating magic link, please wait...');
+// 		generateMagicLink(sessionDetails.access_token)
+// 			.then((res) => {
+// 				console.log('Res - ', res);
+// 				$w('#magicLinkField').value = res.magicLink || '';
+// 				showSuccessMessage(
+// 					'Link generated successfully. Select the prompt and copy it.'
+// 				);
+// 				fetchMagicLinks(sessionDetails.access_token);
+// 			})
+// 			.catch((err) => {
+// 				console.log('Error - ', err);
+// 				showErrorMessage(err.message);
+// 			});
+// 	});
+// 	fetchMagicLinks(sessionDetails.access_token);
+
+// 	$w('#tableContent').onItemReady(($item, itemData, index) => {
+// 		$item('#serialNumberField').text = `${index + 1 < 10 ? '0' : ''}${
+// 			index + 1
+// 		}`;
+// 		$item('#magicCodeField').text = itemData.employeeCode;
+// 		$item('#revokeButton').onClick(() => {
+// 			$item('#revokeButton').hide();
+// 			$item('#revokingText').show();
+// 			revokeAMagicCode(sessionDetails.access_token, itemData.employeeCode)
+// 				.then((res) => {
+// 					if (!res.error) {
+// 						showSuccessMessage(res.message);
+// 						fetchMagicLinks(sessionDetails.access_token);
+// 					} else {
+// 						if (res.message === '401:jwt expired') wixLocation.to('/login');
+// 						console.log('Comes here - ', res.message);
+// 						showErrorMessage(res.message);
+// 					}
+// 					$item('#revokingText').hide();
+// 					$item('#revokeButton').show();
+// 				})
+// 				.catch((err) => {
+// 					showErrorMessage(err.message);
+// 					$item('#revokeButton').show();
+// 				});
+// 		});
+// 	});
+// 	$w('#tableContent').data = magicLinks;
+// });
+
+const initSession = () => {
 	const userDetails = JSON.parse(session.getItem('user'));
 	const sessionDetails = JSON.parse(session.getItem('session'));
 	if (!userDetails || !sessionDetails) return wixLocation.to('/login');
-
+	if (!userDetails.user_metadata.orgAdmin)
+		return wixLocation.to('/dashboard-users');
 	$w('#nameField').text = `Hello ${
 		userDetails.user_metadata.firstName ?? 'Guest'
 	}`;
+	return sessionDetails.access_token;
+};
 
-	$w('#generateMagicLink').onClick(() => {
-		showGeneralMessage('Generating magic link, please wait...');
-		generateMagicLink(sessionDetails.access_token)
-			.then((res) => {
-				console.log('Res - ', res);
-				$w('#magicLinkField').value = res.magicLink || '';
-				showSuccessMessage(
-					'Link generated successfully. Select the prompt and copy it.'
-				);
-				fetchMagicLinks(sessionDetails.access_token);
-			})
-			.catch((err) => {
-				console.log('Error - ', err);
-				showErrorMessage(err.message);
-			});
+const getData = (token) =>
+	getAllAccessesRequest(token).then(({ data, error, redirect, message }) => {
+		if (error) return showErrorMessage(message);
+		if (redirect) return wixLocation.to('/login');
+		if (data) {
+			$w('#tableContent').data = data.map((item) => ({
+				...item,
+				_id: item.requestID,
+			}));
+		}
 	});
-	fetchMagicLinks(sessionDetails.access_token);
 
-	$w('#tableContent').onItemReady(($item, itemData, index) => {
-		$item('#serialNumberField').text = `${index + 1 < 10 ? '0' : ''}${
-			index + 1
-		}`;
-		$item('#magicCodeField').text = itemData.employeeCode;
-		$item('#revokeButton').onClick(() => {
-			$item('#revokeButton').hide();
-			$item('#revokingText').show();
-			revokeAMagicCode(sessionDetails.access_token, itemData.employeeCode)
-				.then((res) => {
-					if (!res.error) {
-						showSuccessMessage(res.message);
-						fetchMagicLinks(sessionDetails.access_token);
-					} else {
-						if (res.message === '401:jwt expired') wixLocation.to('/login');
-						console.log('Comes here - ', res.message);
-						showErrorMessage(res.message);
-					}
-					$item('#revokingText').hide();
-					$item('#revokeButton').show();
+$w.onReady(function () {
+	const token = initSession();
+	getData(token);
+
+	$w('#tableContent').onItemReady(($item, data) => {
+		$item('#primaryCategoryField').text = data.primaryCategory;
+		$item('#secondaryCategoryField').text = data.secondaryCategory;
+		$item('#userIDField').text = data.userID;
+		$item('#remarksField').text = data.remarks;
+		$item('#approveButton').onClick(() => {
+			$item('#approvingMessageField').text = 'Approving, please wait...';
+			$item('#approvingMessageField').show();
+			$item('#approveButton').hide();
+
+			approveAccessByRequestID(token, data.requestID)
+				.then(({ error, message, redirect }) => {
+					if (redirect) return wixLocation.to('/login');
+					if (error) return showErrorMessage(message);
+					$item('#approvingMessageField').hide();
+					getData(token);
+					return showSuccessMessage('Successfully approved the request');
 				})
 				.catch((err) => {
-					showErrorMessage(err.message);
-					$item('#revokeButton').show();
+					$item('#approvingMessageField').hide();
+					$item('#approveButton').show();
+					return showErrorMessage(err.message);
 				});
 		});
 	});
-	$w('#tableContent').data = magicLinks;
+
+	$w('#tableContent').data = [];
 });
